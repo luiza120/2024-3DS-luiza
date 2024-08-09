42566c7d87e9f91f6b75d3222291b5a9d2ef657f
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-09 10:37:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const textoResultado =  document.querySelector('texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            "alternativa01",
+            "alternativas02"
+        ]
+    }
+]
