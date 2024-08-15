d4ce6fe5bf876c67b2d2bc290e442d27899be83b
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-15 10:39:43 -0300

    atualizando

--- main.js
+++ main.js
@@ -74,11 +74,17 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() 
     {
+        if (atual >= perguntas.length){
+            mostraResultado();
+            return
+        }
         perguntaAtual = perguntas[atual];
         caixaPerguntas.textContent = perguntaAtual.enunciado;
+        caixaAlternativas.textContent = ""
         mostraAlternativas();
     }
 
@@ -88,13 +94,20 @@ function mostraPergunta()
         {
             const botaoAlternativas = document.createElement("button");
             botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function()
-            {
-                atual++;
-                mostraPergunta();
-            })
+            botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa))
             caixaAlternativas.appendChild(botaoAlternativas);
         }
+    }
+    function respostaSelecionada(opcaoselecionada){
+        const afirmacoes = opcaoSelecionada.afirmacao;
+        historiaFinal += afirmacoes + " "
+        atual++;
+        mostraPergunta();
+    }
+    function mostraResultado (){
+        caixaPerguntas.textContent = "Inicio do texto..."
+        textoResultado.textContent = historiaFinal
+        caixaAlternativas.textContent = "";
     }
 
     mostraPergunta();
